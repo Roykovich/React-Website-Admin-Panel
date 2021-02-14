39e3fc8950dc981caee305fca4ce1ba4d4bf55cb
Rubén Ortiz <runerd.menethil@gmail.com>
2021-02-13 23:25:24 -0400

    refactor(components) Add progress bar to Header Component
    Add progress bar to the header component. If the current progress it's 0 it will appear 1% in the progress bar.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
 // Create a bar component, maybe an external file
 const Bar = ({ name, progress }) => {
   return (
-    <div className="bar-container">
+    <div className="book-progress-bar">
       <p className="bar-title">{name}</p>
-      <div className="bar">
-        <div className="bar-progress"></div>
+      <div className="bar-container">
+        <div className="bar">
+          <div
+            className="bar-progress"
+            style={{ width: progress === 0 ? 1 : progress + "%" }}
+          ></div>
+        </div>
         <p>{progress}%</p>
       </div>
     </div>
@@ -42,18 +47,42 @@ const UnauthenticatedHeader = () => {
     <>
       {/* Aquí una imagen? */}
       <div className="about">
-        <h3>Username here</h3>
-        <h4>@username</h4>
+        <div className="about-container">
+          <div className="about-img">
+            <img
+              src="https://randomuser.me/api/portraits/men/34.jpg"
+              alt="John Doe Photo Portrait"
+            />
+          </div>
+          <div className="about-text">
+            <h2>STATE OF THE JOHN DOE</h2>
+            <br />
+            <br />
+            <p>Aquí un textico muy cosmico que cosmiquea en las interwebs</p>
+            <br />
+            <h4 className="read-more">Read more</h4>
+          </div>
+        </div>
       </div>
       <div className="info">
-        <div className="events"></div>
-        <div className="updates"></div>
+        <div>
+          <h2>Events</h2>
+          <p>Peace was never an option. Cunt.</p>
+          <h4 className="read-more">Read more</h4>
+        </div>
+        <div>
+          <h2>Lastest Updates</h2>
+          <p>Peace was never an option. Cunt.</p>
+          <h4 className="read-more">Read more</h4>
+        </div>
       </div>
       <div className="progressBars">
-        <Bar name={"Fahrenheit 451"} progress={63} />
-        <Bar name={"Rogue Protocols"} progress={100} />
-        <Bar name={"Rythm of War"} progress={0} />
-        <Bar name={"Rythm of War"} progress={0} />
+        <div className="progressBars-container">
+          <Bar name={"Fahrenheit 451"} progress={63} />
+          <Bar name={"Rogue Protocols"} progress={100} />
+          <Bar name={"Rythm of War"} progress={0} />
+          <Bar name={"Mistborn"} progress={16} />
+        </div>
       </div>
     </>
   );

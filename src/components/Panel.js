df659f7e3f02dcc3ea13aad9fda67c3769965284
Rubén Ortiz <runerd.menethil@gmail.com>
2021-03-06 21:06:48 -0400

    Create Panel.js in order to add the admin Panel

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,11 +1,54 @@
 import { Link } from "react-router-dom";
+import { useState } from 'react'
+
+import "../styles/Panel.css";
+
 
 const Panel = () => {
+  const [active, setActive] = useState('New Post');
+
+  const makeActive = option => {
+    setActive(option)
+  }
+
+  const options = [
+    'New Post',
+    'Edit Page',
+    'Analytics'
+  ]
+
   return (
-    <div>
-      <h4>Admin Panel</h4>
-      <Link to="/">Pa tras</Link>
-    </div>
+    <main className="main-panel">
+      <div className="admin">
+        <div className="menu">
+          <div className="options">
+            <h3>John Doe</h3>
+            <ul>
+              {
+                options.map((item, i) => {
+                  return <li
+                    className={active === item ? 'active' : {}}
+                    onClick={() => makeActive(item)}
+                    key={i}
+                  >
+                    <a>{item}</a>
+                  </li>
+                })
+              }
+            </ul>
+          </div>
+          <p>Website Admin v0.1.0</p>
+        </div>
+        <div className="settings">
+          {
+            active === 'New Post' ? <h1>{active}</h1> :
+              active === 'Edit Page' ? <h1>{active}</h1> :
+                active === 'Analytics' ? <h1>{active}</h1> :
+                  <h1 style={"color: red"}>Error</h1>
+          }
+        </div>
+      </div>
+    </main>
   );
 };
 

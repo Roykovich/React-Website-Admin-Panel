8078f6181625aa97cf413d2f5fd578b85c908828
Rubén Ortiz <runerd.menethil@gmail.com>
2021-03-14 19:47:47 -0400

    Create editPage component and add onChange method to the inputs on createPost component

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MaybePost } from "../components/Content";
-
+import Header from "../components/Header";
 
 /**
  * TODO
@@ -8,42 +8,76 @@ import { MaybePost } from "../components/Content";
  *  * 2) Cambiar el texto del preview con state
  */
 
-export const EditPage = () => {
+export const EditPage = (props) => {
+  const [events, setEvents] = useState(props.events);
+  const [updates, setUpdates] = useState(props.updates);
+  const [bookProgress, setBookProgress] = useState(props.bookProgress);
+
+  const limits = {
+    event: 28,
+  };
+
+  const changeEvents = (event) => {
+    setEvents(event.target.value);
+  };
+
   return (
     <>
-      <button>touch me</button>
+      <div>
+        <Header events={events} updates={updates} bookProgress={bookProgress} />
+      </div>
+      <div>
+        <form action="" method="post">
+          <label htmlFor="Events">
+            <b>Events</b>
+            <div>
+              <input
+                type="text"
+                name="Events"
+                id=""
+                placeholder="Type a title"
+                value={events}
+                onChange={changeEvents}
+              />
+              <p>{limits.event - events.length}</p>
+            </div>
+          </label>
+        </form>
+      </div>
     </>
-  )
-}
-
+  );
+};
 
 export const NewPost = () => {
-  let today = new Date();
-  let dd = String(today.getDate()).padStart(2, "0");
-  let mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
-  let yyyy = today.getFullYear();
-
-  today = mm + "/" + dd + "/" + yyyy;
-
   const [title, setTitle] = useState("Título");
-  const [date, setDate] = useState(today);
   const [content, setContent] = useState(
     "Lorem, ipsum dolor sit amet consectetur adipisicing elit."
   );
+
+  const titleChange = (event) => {
+    setTitle(event.target.value);
+  };
 
   return (
     <>
       <h1>Create a new post</h1>
       <div className="preview">
-        <MaybePost title={title} date={date} content={content} width={100} />
+        <MaybePost title={title} content={content} width={100} />
       </div>
       <div className="createPost">
         <form action="POST">
-          <label for="Título">
+          <label htmlFor="Título">
             <b>Título</b>
           </label>
-          <input type="text" name="Título" id="" placeholder="Type a title" />
-          <label for="Introducción">
+          <input
+            type="text"
+            name="Título"
+            id=""
+            placeholder="Type a title"
+            value={title}
+            onChange={titleChange}
+          />
+          <label htmlFor="Introducción">
             <b>Introducción</b>
           </label>
           <textarea

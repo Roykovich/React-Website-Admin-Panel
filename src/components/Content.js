5070b2144d5872abd7bdbabca05a6b52f5e6d7f6
Rubén Ortiz <runerd.menethil@gmail.com>
2021-11-25 22:57:34 -0300

    Refactor MaybePost component structure

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,31 +1,25 @@
 import { HiChevronDoubleRight } from "react-icons/hi";
-import '../styles/Content.css'
+import "../styles/Content.css";
 
-
-export const MaybePost = props => {
+export const MaybePost = ({ src, title, content }) => {
   return (
-    <div className="post" style={{ width: props.width + "%" }}>
+    <article className="post" style={{ minWidth: "100%" }}>
       <div className="post-container">
-        <img src="https://picsum.photos/300/200" alt="" />
+        <img src={src} alt="" />
         <div className="preview">
-          <h2>{props.title}</h2>
-          <p>fecha acá</p>
+          <h2 class="post-title">{title}</h2>
+          <p className="post-date">Date Here</p>
           <br />
-          <h4>
-            {props.content}
-          </h4>
+          <p className="post-description">{content}</p>
           <br />
-          <h4 className="read-more">
-            Read more
-            <HiChevronDoubleRight />
-          </h4>
+          <h4 className="read-more">Read more</h4>
         </div>
       </div>
-    </div>
+    </article>
   );
-}
+};
 
-const Post = ({src, description}) => {
+const Post = ({ src, description }) => {
   return (
     <article className="post">
       <div className="post-container">
@@ -36,9 +30,7 @@ const Post = ({src, description}) => {
           <br />
           <p className="post-description">{description}</p>
           <br />
-          <h4 className="read-more">
-            Read more
-          </h4>
+          <h4 className="read-more">Read more</h4>
         </div>
       </div>
     </article>
@@ -48,19 +40,19 @@ const Post = ({src, description}) => {
 const Content = () => {
   return (
     <main className="main-content">
-      <Post 
+      <Post
         src="https://picsum.photos/600/400?random=1"
         description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus debitis voluptate, itaque pariatur sunt repellendus sed tempore harum beatae repellat."
       />
-      <Post 
+      <Post
         src="https://picsum.photos/600/400?random=2"
         description="Ipsa cupiditate laudantium nobis optio hic tenetur explicabo? Eaque, doloribus! Quo consequatur, ex ad non quasi commodi ducimus sed amet!"
       />
-      <Post 
+      <Post
         src="https://picsum.photos/600/400?random=3"
         description="Aspernatur consequuntur optio fuga aliquam non recusandae vel, pariatur neque iure unde veritatis repellendus nesciunt sunt. Quos voluptate quis quibusdam?"
       />
-      <Post 
+      <Post
         src="https://picsum.photos/600/400?random=4"
         description="empora libero totam dignissimos alias saepe omnis in delectus magni recusandae blanditiis dicta eum debitis eveniet, placeat sunt dolorum quis."
       />
